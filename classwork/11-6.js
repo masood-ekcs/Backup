f54534455dba34d7490e1f2951cf94d8d93a1ea0
masood-ekcs <mahmad@ekcs.co>
2022-06-11 10:46:20 +0530

    11-6

---
+++ classwork/11-6.js
@@ -0,0 +1,36 @@
+console.log("Hello!")
+var a = 400
+var b = 9999999
+var c = a + b
+console.log(c)
+var d = 5/2
+console.log(d)
+var e = a**b
+console.log(e)
+console.log(5e2)
+console.log(2e1)
+console.log(1e1)
+
+//
+//
+//compound operators
+var a = 10
+var b = 20
+//console.log(a)
+//console.log(b)
+// a+=b ---> a = a+b
+// a-=b ---> a = a-b
+//a+=b
+//console.log(a)
+
+// a++ ---> a = a+1
+//a++
+//console.log(++a)
+console.log(a)
+
+//
+var c = a++
+console.log(c)
+
+//
+console.log(5&2)
