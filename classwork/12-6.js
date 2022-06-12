b1490756788cc69c534ce0bd94fe66ef23f03d9c
Ayush <kayush479@gmail.com>
2022-06-12 10:02:52 +0530

    Added bulb project

--- classwork/12-6.js
+++ classwork/12-6.js
@@ -1,1 +1,69 @@
+/* left shift opeartor 
 console.log(1<<2)
+
+m<<k
+
+Add k zeros in binary representation of m
+If it equivalent to m x 2^k
+
+
+Right shift opeartor 
+m>>k
+
+remove last k bits and it is equivalent to integral part of m/2^k
+
+*/
+
+/* Comparision Opeartors/ Conditional Operators
+
+  == - Euality
+  === - Euality as well as data type equality
+  > - Greater Than
+  < - Less than
+  >= - Greater than or equal to
+  <= - Less than equal to
+  && - Multiple true
+  || - Any one of true
+
+Type 1. - Normal if block
+Type 2. - If else
+Type 3. - If - else if block
+
+*/
+
+/*
+var name = "Ayush"
+
+if(name=="Ayush")
+{
+    console.log(1)
+}
+else if(name=="Chandan")
+{
+    console.log(2)
+}
+else if(name=="Masood")
+{
+    console.log(3)
+
+}
+else
+{
+    console.log(4)
+}
+
+
+*/
+
+
+var a = "Ayush"
+var b = "male"
+
+if(a=="Ayush" && b=="male")
+{
+
+
+}
+
+
+
