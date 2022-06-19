edcf6b1269053c9e0585982e926ce2e02d4d6c5c
masood-ekcs <mahmad@ekcs.co>
2022-06-19 10:56:15 +0530

    19-6

---
+++ projects/Masood/19-6.js
@@ -0,0 +1,10 @@
+//check if variable is equal to Ayush and age 28 and weight >65
+var name = "Sunil"
+var age = 25
+var weight = 65
+if (name == "Ayush" && age == 28 && weight >= 65) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
