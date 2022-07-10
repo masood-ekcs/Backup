89e8f03dbebd854f90633b815cf7bf01110573dc
Sunil470 <sunil47040@gmail.com>
2022-07-10 03:31:21 -0400

    sunil-10-7-2022

---
+++ classwork/Sunil/02-07.js
@@ -0,0 +1,70 @@
+// for(i=1;i<=20;i++){
+//     console.log(i)
+// }
+/*print number 10 to 50 , that number+3 */
+// for(i=10;i<=50;i=i+3){
+//     console.log(i)
+// }
+
+/*print number 1 to 1000 which divide by 3 and 5 */
+// for(var i=1;i<=1000;i++){
+//     if(i%3==0 && i%5==0){
+//         console.log(i)
+//     }
+// }
+// var num = 230
+// count= 0
+// for (var i=1;i<=num;i++){
+//   if(num%i==0){
+// count++
+//   }
+// }
+// console.log(count)
+
+
+// var num = 2468513
+// count = 0
+// for(; num>0; ){
+//     num = Math.floor(num/10)
+//     count++
+// }
+//console.log(count)
+
+// var num  = 54321
+// var count = 0;
+
+// var ans = ""
+
+// for(;num>0;)
+//   {
+//     var digit =  num%10;
+//     num = Math.floor(num/10);
+//      ans = ans + digit.toString()
+//   }
+//   console.log(parseInt(ans))
+
+//Print even factors of a given number
+var num = 1024
+for(var i=1; i<=num; i++){
+  if (num%i==0 &&i%2==0){ 
+      console.log(i)
+     
+   
+  }
+}
+//console.log(factorCount)
+//console.log(evenFactor)
+
+
+console.log("\n\n\n")
+//-------------------------------------------------------------//
+// Print number of factors that are multiple of 3
+var num = 12345
+var count = 0
+  for(i=1;i<=num;i++){
+     if(num%i==0 && i%3==0)
+     {
+       console.log(i);
+     }
+  
+  }
--- classwork/Sunil/even_armstrong_number.js
+++ classwork/Sunil/even_armstrong_number.js
@@ -27,6 +27,27 @@ for(var i=2; i<num; i++){
         isPrime = false
     }}
     if(isPrime = true){
-        console
-    } 
+        console.log("its prime number")
+    }
+ //Prime Number   
+    var count = 0;
+
+    for(var num=3;num<=100;num++){
+              var isPrime = 1 //assuming prime is true
+              for(var i =2; i<num; i++)
+                {
+                  if(num%i==0)
+                  {
+                    isPrime = 0; // if false
+                   
+                  }
+                }
+          if(isPrime)
+          {  count++;
+            console.log(num)
+            
+          }
+      
+    }
+    console.log(count)
 
